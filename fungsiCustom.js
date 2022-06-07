e408d2a8361bd44a3b13baf5e1028296afa4b9f5
Bardizba Z <moch.bardizba@gmail.com>
2022-06-07 21:37:24 +0700

    feat: read json file

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -16,9 +17,32 @@ let modifyFile3 = (val) => {
   file3 = val;
 };
 
+const readJson = (file_path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file_path, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(JSON.parse(data.toString()))
+      }
+    })
+  })
+}
+
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fn) => {
+  Promise.all([
+    readJson(file1),
+    readJson(file2),
+    readJson(file3),
+  ]).then((result) => {
+    const messages = result.map( (json) => (json.message || json[0].message || json[0].data.message || "").split(" ")[1])
+    fn(null, messages)
+  }).catch((err) => {
+    fn(err, null)
+  })
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
